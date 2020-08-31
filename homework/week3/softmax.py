21d44f05c676664f5a7e1d6256edc4ef023b5f90
ouyangzizhou <ouyangthu2015@163.com>
2020-08-31 09:20:11 +0800

    '添加nn和functional区别的'

--- homework/week3/softmax.py
+++ homework/week3/softmax.py
@@ -3,17 +3,17 @@ import torch
 
 def softmax_np(logits):
 	assert (isinstance(logits, np.ndarray)), 'only numpy is available'
-	delta = 1e-7
-	exp_value = np.exp(logits) + delta
+	exp_value = np.exp(logits)
 	dim_ext = np.sum(exp_value, 1).reshape(-1, 1)
-	return exp_value / dim_ext
+	log_res = torch.log(exp_value) - torch.log(dim_ext)
+	return np.exp(log_res)
 
 def softmax_torch(logits):
 	assert (isinstance(logits, torch.Tensor)), 'only torch is available'
-	delta = 1e-7
-	exp_value = torch.exp(logits) + delta
+	exp_value = torch.exp(logits)
 	dim_ext = torch.sum(exp_value, 1).reshape(-1, 1)
-	return exp_value / dim_ext
+	log_res = torch.log(exp_value) - torch.log(dim_ext)
+	return np.exp(log_res)
 
 logits_np = np.random.rand(3,5)
 s_np = softmax_np(logits_np)
