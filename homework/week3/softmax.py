61791bbffaf158ccd114fc368d7f0352ec6475d7
ouyangzizhou <ouyangthu2015@163.com>
2020-09-08 16:02:22 +0800

    '添加防止exp计算溢出的代码'

--- homework/week3/softmax.py
+++ homework/week3/softmax.py
@@ -3,17 +3,23 @@ import torch
 
 def softmax_np(logits):
 	assert (isinstance(logits, np.ndarray)), 'only numpy is available'
-	exp_value = np.exp(logits)
+	C = np.exp(-np.max(logits)) # 防止溢出的操作系数
+	exp_value = np.exp(logits) # 
 	dim_ext = np.sum(exp_value, 1).reshape(-1, 1)
-	log_res = torch.log(exp_value) - torch.log(dim_ext)
-	return np.exp(log_res)
+	# log_res = torch.log(exp_value) - torch.log(dim_ext)
+	res = C * exp_value / ( C * dim_ext )
+
+	return res
 
 def softmax_torch(logits):
 	assert (isinstance(logits, torch.Tensor)), 'only torch is available'
+	C = np.exp(-torch.max(logits)) # 防止溢出的操作系数
 	exp_value = torch.exp(logits)
 	dim_ext = torch.sum(exp_value, 1).reshape(-1, 1)
-	log_res = torch.log(exp_value) - torch.log(dim_ext)
-	return np.exp(log_res)
+	# print(dim_ext) 一列
+	res = (C * exp_value) / ( C * dim_ext )
+
+	return res
 
 logits_np = np.random.rand(3,5)
 s_np = softmax_np(logits_np)
